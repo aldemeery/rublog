420db934cf5482841bc0232c3952eae94b178c8d
Osama Aldemeery <aldemeery@gmail.com>
2020-07-11 00:14:13 +0200

    Add image uploads to articles.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 # rubocop:disable Style/NumericLiterals
-ActiveRecord::Schema.define(version: 2020_07_10_200743) do
+# rubocop:disable Metrics/BlockLength
+ActiveRecord::Schema.define(version: 2020_07_10_220157) do
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.text 'body'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.string 'image_file_name'
+    t.string 'image_content_type'
+    t.integer 'image_file_size'
+    t.datetime 'image_updated_at'
   end
 
   create_table 'comments', force: :cascade do |t|
@@ -48,3 +53,4 @@ ActiveRecord::Schema.define(version: 2020_07_10_200743) do
   add_foreign_key 'taggings', 'tags'
 end
 # rubocop:enable Style/NumericLiterals
+# rubocop:enable Metrics/BlockLength
